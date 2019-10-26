809a085a9ca8ed54a890bc26d103752484b696ce
Michael Frantz <mjfrantz26@gmail.com>
2019-10-26 22:11:31 +0900

    Comp Report

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ var app = express(); //create an express app
 
 // Enable CORS 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
 
 });
 
@@ -29,8 +29,7 @@ app.set('view engine', ejs);
 
 // Mongo & mongoose
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://ThiIsAPassword:TheRealPassword@cluster0-shard-00-00-euadh.mongodb.net:27017,cluster0-shard-00-01-euadh.mongodb.net:27017,cluster0-shard-00-02-euadh.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin' 
-    );
+mongoose.connect('mongodb://ThiIsAPassword:TheRealPassword@cluster0-shard-00-00-euadh.mongodb.net:27017,cluster0-shard-00-01-euadh.mongodb.net:27017,cluster0-shard-00-02-euadh.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
 
 // Db connection
 var db = mongoose.connection;
@@ -45,84 +44,127 @@ var nextId = 0;
 // Web Server fucntionality 
 
 app.get('/', (req, res) => {
-    res.render('index.html');
+	res.render('index.html');
 });
 
 app.get('/admin', (req, res) => {
-    res.render('admin.html');
+	res.render('admin.html');
 });
 
 app.get('/contact', (req, res) => {
-    res.send('This is the contact page');
+	res.send('This is the contact page');
 });
 
 app.get('/about', (req, res) => {
-    res.render('about.html');
+	res.render('about.html');
 });
 
 // Endpoint of REST Functionality
-
 app.get('/api/products', (req, res) => {
-    itemDB.find({}, function(error, data){
-        if(error){
-            console.log("Error reading data", error)
-            res.status(500);
-            res.send(error);
-        }
-        res.json(data);
-    });
+	itemDB.find({}, function (error, data) {
+		if (error) {
+			console.log("Error reading data", error)
+			res.status(500);
+			res.send(error);
+		}
+		res.json(data);
+	});
 });
 
 app.get('/api/products/:user', (req, res) => {
-    itemDB.find({ price : { $gte: 10 }}, function(error, data){
-        if(error){
-            console.log("Error reading data", error)
-            res.status(500);
-            res.send(error);
-        }
-        res.json(data);
-    });
+	itemDB.find({
+		user: req.params.user
+	}, function (error, data) {
+		if (error) {
+			console.log("Error reading data", error)
+			res.status(500);
+			res.send(error);
+		}
+		res.json(data);
+	});
 });
 
-app.post('/api/products', (req, res) => {
-    //get and assign ID  
-    var item = new itemDB(req.body);
+app.get("/api/remove/:id", (req, res) => {
+	itemDB.deleteMany({
+		id: req.params.id
+	}, function (error, data) {
+		if (!error) {
+			res.send("items removed!");
+			return;
+		}
 
-    // save the object to Db
-    item.save(function(error, savedItem){
-        if(error){
-            console.log("Error, item was not saved on Mongo", error);
-            res.status(500);
-            res.send(error);
-        }
-        console.log("Item saved!");
-        //send as a JSON
-        res.status = 201;
-        res.json(savedItem);
-    });
+		res.send("Error");
+	});
+});
+
+app.get("/api/removeMyItems", (req, res) => {
+	itemDB.deleteMany({
+		user: 'Mike'
+	}, function (error, data) {
+		if (!error) {
+			res.send("items removed!");
+			return;
+		}
+
+		res.send("Error");
+	});
+});
+
+function isItValidNumber(num) {
+	if (parseFloat(num) == num || Number.isInteger(num)) {
+		return true;
+	}
+	return false;
+};
+
+app.post('/api/products', (req, res) => {
+	//perform validations
+	if (!isItValidNumber(req.body.price) || !isItValidNumber(req.body.stock)) {
+		//error on received data
+		res.status = 400;
+		res.send("Error on data type for Price or Stock. Correct and retry")
+	}
+
+	//get and assign ID  
+	var item = new itemDB(req.body);
+	// save the object to Db
+	item.save(function (error, savedItem) {
+		if (error) {
+			console.log("Error, item was not saved on Mongo", error);
+			res.status(500);
+			res.send(error);
+		}
+		console.log("Item saved!");
+		//send as a JSON
+		res.status = 201;
+		res.json(savedItem);
+	});
 });
 
 //Listen to db connection events
 db.on('error', function (error) {
-    console.log("Error connection to Mongo Server", error);
+	console.log("Error connection to Mongo Server", error);
 });
 
-db.on('open', function(){
-    console.log('Db is alive');
+db.on('open', function () {
+	console.log('Db is alive');
 
-    // Schemas
-    var itemSchema = mongoose.Schema({
-        title: String,
-        description: String,
-        price: Number,
-        image: String,
-        category: String,
-        user: String
-    });
+	// Schema
+	var itemSchema = mongoose.Schema({
+		title: String,
+		description: String,
+		price: Number,
+		image: String,
+		category: String,
+		user: String,
+		stock: Number,
+		priority: Number
 
-    itemDB = mongoose.model("itemsCh4", itemSchema);
+	});
+
+	itemDB = mongoose.model("itemsCh4", itemSchema);
 });
 
 app.listen(8080, function () {
-    console.log('Server listening on http://localhost:8080');
-}); 
+	console.log('Server listening on http://localhost:8080');
+});
--- views/scripts/admin.js
+++ views/scripts/admin.js
@@ -1,68 +1,86 @@
-
 //Object Constructor
-function Item(title, description, price, image, category){
-    this.title = title;
-    this.description = description;
-    this.price = price;
-    this.image = image;
-    this.category = category;
-    this.user = "Mike";
+function Item(title, description, price, image, category, stock, priority) {
+	this.title = title;
+	this.description = description;
+	this.price = price;
+	this.image = image;
+	this.category = category;
+	this.stock = stock;
+	this.priority = priority;
+	this.user = "Mike";
 }
 
 function saveItem() {
-    //get data 
-    var title = $("#titleTxt").val();
-    var description = $("#descriptionTxt").val();
-    var price = $("#priceTxt").val();
-    var image = $("#imageTxt").val();
-    var category = $("#selCategory").val();
+	//get data 
+	var title = $("#titleTxt").val();
+	var description = $("#descriptionTxt").val();
+	var price = $("#priceTxt").val();
+	var image = $("#imageTxt").val();
+	var category = $("#selCategory").val();
+	var stock = $("#txtStock").val();
+	var priority = $("#selPriority").val();
 
-    console.log(title, description, price, image, category);
+	console.log(title, description, price, image, category, stock, priority);
 
-    // create object 
-    var theItem = new Item(title, description, price, image, category);
-    console.log(theItem);
-    //Read the obj
-    console.log("the title: ", theItem.title)
-    
-    //save object to backend
-    var serverURL = "http://127.0.0.1:8080/api/products"
-    $.ajax({
-        url: serverURL,
-        type: "POST",
-        data: JSON.stringify(theItem),
-        contentType: "application/json",
-        success:function(res){
-            console.log("Req successful", res);
-        },
-        error:function(error){
-            console.error("Error on req", error);
-        }
-    });
+	// create object 
+	var theItem = new Item(title, description, price, image, category, stock, priority);
+	console.log(theItem);
+	//Read the obj
+	console.log("the title: ", theItem.title)
+
+	//save object to backend
+	var serverURL = "http://127.0.0.1:8080/api/products"
+	$.ajax({
+		url: serverURL,
+		type: "POST",
+		data: JSON.stringify(theItem),
+		contentType: "application/json",
+		success: function (res) {
+			console.log("Req successful", res);
+		},
+		error: function (error) {
+			console.error("Error on req", error);
+		}
+	});
 }
 
-function testAjax(){
-    var serverURL = "http://restclass.azurewebsites.net/API/test";
+function testAjax() {
+	var serverURL = "http://restclass.azurewebsites.net/API/test";
 
-    $.ajax({
-        url:serverURL,
-        type:"GET",
-        success:function(res){
-            console.log("Req successful", res);
-        },
-        error:function(error){
-            console.error("Error on req", error);
-        }
-    });
+	$.ajax({
+		url: serverURL,
+		type: "GET",
+		success: function (res) {
+			console.log("Req successful", res);
+		},
+		error: function (error) {
+			console.error("Error on req", error);
+		}
+	});
 }
 
-function init(){
-    console.log("Init");
+function clearDB() {
+	var serverURL = "http://127.0.0.1:8080/api/removeMyItems";
+	$.ajax({
+		url: serverURL,
+		type: "GET",
+		success: function (res) {
+			console.log("Req successful", res);
+		},
+		error: function (error) {
+			console.error("Error on req", error);
+		}
+	});
+}
 
-    //initializations 
+function init() {
+	console.log("Init");
 
-    //events
-    $("#btnSave").click(saveItem);
+	//initializations 
+
+	//events
+	$("#btnSave").click(saveItem);
+	$("#btnClear").click(clearDB);
 }
 
 window.onload = init;
